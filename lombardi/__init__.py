6173ce8b03916ca3eeb436ebbbc0e0050f794845
Connor <loughondata@protonmail.com>
2025-07-27 15:22:22 -0700

    chore: linting, formatting

--- lombardi/__init__.py
+++ lombardi/__init__.py
@@ -2,12 +2,12 @@
 
 __version__ = "0.1.0"
 
-from .analyzers.complexity_analyzer import ComplexityAnalyzer
 from .analyzers.antipattern_detector import AntipatternDetector
+from .analyzers.complexity_analyzer import ComplexityAnalyzer
 from .analyzers.optimization_suggester import OptimizationSuggester
 
 __all__ = [
     "ComplexityAnalyzer",
-    "AntipatternDetector", 
+    "AntipatternDetector",
     "OptimizationSuggester",
 ]
